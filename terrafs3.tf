15ebd61d642cb6b5b93d9b47714166d5c7e5fc00
ukk321 <162373844+ukk321@users.noreply.github.com>
2025-02-24 01:06:36 +0500

    Update terrafs3.tf

--- terrafs3.tf
+++ terrafs3.tf
@@ -3,7 +3,16 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "your-static-website-bucket-name"
+  bucket = "your-static-website-bucket-2011"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
@@ -12,22 +21,24 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   index_document {
     suffix = "terra.html"
   }
-
-  error_document {
-    key = "error.html"
-  }
 }
 
 resource "aws_s3_bucket_policy" "public_access" {
   bucket = aws_s3_bucket.website_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-    }]
-  })
-}
 
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access_block
+  ]
+}
