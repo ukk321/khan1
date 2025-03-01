4fdb6d5c7788003e6fb92f14e578d9c3e6f354a5
ukk321 <info.umar2002@gmail.com>
2025-03-02 00:02:05 +0500

    mystaticapp

---
+++ my-static-app/script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript for interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio website loaded!');
+    // Add more JavaScript functionality here if needed
+});
